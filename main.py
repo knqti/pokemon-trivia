67095b8898b7fb8070157776d0e4bc057367bfa6
knqti <166448899+knqti@users.noreply.github.com>
2026-01-31 17:27:37 -0800

    Create hints and re-organize

--- main.py
+++ main.py
@@ -12,17 +12,18 @@ def ask_question(pokemon: object):
     ability = random.choice(pokemon.abilities)
 
     title = "Who's that POK\u00e9MON?"
+    print()
     print('#' * len(title))
     print(title)
     print('#' * len(title))
 
+    print(f'\n{pokemon.flavor_text}')
     print(f'\n- This POK\u00e9MON is approxiamately {height_ft} ft. tall and weighs about {weight_lb} lb.\n- It is known to have the ability {ability}.\n')
-    print(pokemon.flavor_text)
 
 def check_answer(user_answer: str, correct_answer: str) -> bool:
     user_answer = user_answer
 
-    if user_answer == correct_answer or user_answer == 'help':
+    if user_answer == correct_answer or user_answer == 'answer':
         return True
     
     print('Nope try again.')
@@ -30,6 +31,10 @@ def check_answer(user_answer: str, correct_answer: str) -> bool:
 
 def main_loop():
     while True:
+        user_input = input('\nPress "enter" to continue...')
+        if user_input == 'quit':
+            return
+    
         id = random.randint(1, 151)
         mystery_pokemon = read_pokedex(pokemon_id=id, to_print=False)
 
@@ -40,14 +45,16 @@ def main_loop():
             
             if user_input == 'quit':
                 return
+            elif user_input == 'hint':
+                print(f'\nIts type(s): {", ".join(mystery_pokemon.types)}')
+                continue
 
             if check_answer(
                 user_answer=user_input, 
                 correct_answer=mystery_pokemon.name
             ):
-                print(f"It's...{mystery_pokemon.name.capitalize()}!")
+                print(f"It's...{mystery_pokemon.name.title()}!")
                 sleep(1)
-                input('\nPress "enter" to continue...')
                 break
 
 
@@ -57,13 +64,10 @@ if __name__ == '__main__':
     print('''
 Welcome to POK\u00e9MON Trivia!
 
-To reveal the answer, enter "help".
-To quit the game, enter "quit".
-
+Enter "hint" to see a hint.
+Enter "answer" to reveal the answer.
+Enter "quit" to end the game.
     ''')
 
-    input('Press "enter" to continue...')
-
     main_loop()
-
     print('Goodbye.')
